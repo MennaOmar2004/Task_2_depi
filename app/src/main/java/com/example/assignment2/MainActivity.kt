cd4b60cf4c30f3dcc1f48a409a8f3c2766859414
Menna Omar <manonomar2004@gmil.com>
2024-11-28 07:29:56 -0800

    first commit

---
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.assignment2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.blue
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.assignment2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintlayout)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
